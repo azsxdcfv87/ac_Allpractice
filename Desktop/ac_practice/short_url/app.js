d418a4af0d3a7d7369d472deb990f8e159076f6a
Allen hsu <chaoyu.hsuu@gmail.com>
2022-08-04 22:08:45 +0800

    feat: use handlebars

--- Desktop/ac_practice/short_url/app.js
+++ Desktop/ac_practice/short_url/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', extname: '.handlebars' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send(`This is my first Express Web App`)
+  res.render('index')
 })
 
 app.listen(port, () => {
