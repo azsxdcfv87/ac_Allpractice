1cad5be89de5428d69cb37777e2eff2ecc958bae
Allen hsu <chaoyu.hsuu@gmail.com>
2022-08-05 10:39:41 +0800

    mongoose connected

--- Desktop/ac_practice/short_url/app.js
+++ Desktop/ac_practice/short_url/app.js
@@ -1,10 +1,16 @@
+const { urlencoded } = require('body-parser')
 const express = require('express')
 const exphbs = require('express-handlebars')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
+
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
+
+app.use(express, urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
---
+++ Desktop/ac_practice/short_url/config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('Mongoose error')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('Mongoose Connected')
+})
